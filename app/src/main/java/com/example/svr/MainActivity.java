7d753ab7ae82b7394a57885306b300c8b2600136
paperfrog <paperfrog@naver.com>
2020-12-12 13:15:36 +0900

    Add fragment function
    메인액티비티에 프래그먼트 관련 함수 작성했고 테스트를 위해 녹음 목록, 녹음의 프래그먼트 클래스와 레이아웃 틀만 만들어놓음.

--- app/src/main/java/com/example/svr/MainActivity.java
+++ app/src/main/java/com/example/svr/MainActivity.java
@@ -1,14 +1,59 @@
 package com.example.svr;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MenuItem;
+import com.example.svr.fragment.RecordActionFrag;
+import com.example.svr.fragment.RecordListFrag;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private BottomNavigationView bottomNavigationView;
+    private RecordListFrag recordListFrag;
+    private RecordActionFrag recordActionFrag;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recordActionFrag = new RecordActionFrag();
+        recordListFrag = new RecordListFrag();
+        bottomNavigationView = findViewById(R.id.bottomNavi);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener()
+        {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.record_list:
+                        onFragmentChanged(0);
+                        break;
+                    case R.id.bookmark:
+                        onFragmentChanged(1);
+                        break;
+                    case R.id.record_action:
+                        onFragmentChanged(2);
+                        break;
+                    case R.id.schedule:
+                        onFragmentChanged(3);
+                        break;
+                    case R.id.action_setting:
+                        onFragmentChanged(4);
+                        break;
+                }
+                return true;
+            }
+            });
+        onFragmentChanged(0);
+    }
+    public void onFragmentChanged(int index){
+        System.out.println("index : "+index);
+        switch (index) {
+            case 0:
+                getSupportFragmentManager().beginTransaction().replace(R.id.Main_Frame,recordListFrag).commit();
+                break;
+            case 2:
+                getSupportFragmentManager().beginTransaction().replace(R.id.Main_Frame,recordActionFrag).commit();
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/example/svr/fragment/RecordListFrag.java
@@ -0,0 +1,16 @@
+package com.example.svr.fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import com.example.svr.R;
+public class RecordListFrag extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.record_list_frag, container, false);
+        return rootView;
+    }
+}
