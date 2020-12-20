bc92072ae89169ad5c99e2d6cf62fa595decb2c3
paperfrog <paperfrog@naver.com>
2020-12-20 22:13:55 +0900

    Add record time func and modify record action design
    이제 좀 있어 보이는 어플로 바뀌어가고 있다.

--- app/src/main/java/com/example/svr/MainActivity.java
+++ app/src/main/java/com/example/svr/MainActivity.java
@@ -47,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
             }
             });
         onFragmentChanged(0);
-        
+
             RecordDB.getInstance().initDB(this);
     }
     public void createDatabase(){
--- app/src/main/java/com/example/svr/fragment/RecordActionFrag.java
+++ app/src/main/java/com/example/svr/fragment/RecordActionFrag.java
@@ -1,14 +1,25 @@
 package com.example.svr.fragment;
+import android.annotation.SuppressLint;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import com.example.svr.R;
 import com.example.svr.RecordDB;
@@ -22,10 +33,16 @@ import java.util.Date;
 public class RecordActionFrag extends Fragment {
     MediaRecorder myAudioRecorder;
     MediaPlayer mediaPlayer;
-    String file_length;
+    Thread timeThread;
     String file_name;
     String record_date;
     SimpleDateFormat simpleDate;
+    boolean isRecord=false;
+    boolean isStart=false;
+    ImageButton recordBtn;
+    ImageButton stopBtn;
+    ToggleButton bookmarkBtn;
+    TextView recordTimeTv;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -34,41 +51,47 @@ public class RecordActionFrag extends Fragment {
         File file= new File(file_path);
         Long date=new Date().getTime();
         Date current_time = new Date(Long.valueOf(date));
+        timeThread=new Thread(new timeThread());
         simpleDate=new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-        Button recordBtn=rootView.findViewById(R.id.btn_record);
-        Button stopBtn=rootView.findViewById(R.id.btn_stop);
+        // init
+        recordBtn=rootView.findViewById(R.id.btn_record);
+        stopBtn=rootView.findViewById(R.id.btn_stop);
+        bookmarkBtn=rootView.findViewById(R.id.btn_record_bookmark);
+        recordTimeTv=rootView.findViewById(R.id.tv_record_time);
+
         stopBtn.setEnabled(false);
+        bookmarkBtn.setChecked(false);
         if (!file.exists()){
             file.mkdirs();
         }
-        file_name=file+"/"+current_time;
+        file_name=file+"/"+current_time+".3gp";
+        setMediaRecorder();
         recordBtn.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
             @Override
             public void onClick(View v) {
-                myAudioRecorder = new MediaRecorder();
-                myAudioRecorder.setOutputFile(file_name);
-                myAudioRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-                myAudioRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-                myAudioRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
-                try {
-                    myAudioRecorder.prepare();
+                if(isRecord){
+                    myAudioRecorder.pause();
+                    recordBtn.setImageResource(R.drawable.record);
+                    isRecord=false;
                 }
-                catch (IllegalStateException ise){
-                    System.out.println("씨발 뭐여 : "+ise);
+                else{
+                    if(isStart)
+                        myAudioRecorder.resume();
+                    else
+                        mediaStart();
+                    recordBtn.setImageResource(R.drawable.pause);
+
+                    isRecord=true;
                 }
-                catch (IOException ioe){
-                    System.out.println("씨발 뭐여 : "+ioe);
-                }
-                mediaStart();
-                recordBtn.setEnabled(false);
                 stopBtn.setEnabled(true);
             }
         });
+
         stopBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mediaPlayer =new MediaPlayer();
-                System.out.println("sssss:"+file_name);
                 try
                 {
                     myAudioRecorder.stop();
@@ -79,58 +102,79 @@ public class RecordActionFrag extends Fragment {
                 }
                 myAudioRecorder.reset();
                 myAudioRecorder.release();
+                timeThread.interrupt();
                 myAudioRecorder=null;
-               recordBtn.setEnabled(true);
-               stopBtn.setEnabled(false);
-                file_length=getLength();
-                record_date=simpleDate.format(new Date(System.currentTimeMillis()));
-                System.out.println(record_date);
-               RecordDB.getInstance().insertRecord(file_name,record_date,file_length,0,file_name);
+                recordBtn.setEnabled(true);
+                stopBtn.setEnabled(false);
+                int isBookmark=0;
+                if(bookmarkBtn.isChecked())
+                    isBookmark=1;
+                String file_length=recordTimeTv.getText().toString();
+                String record_date=simpleDate.format(new Date(System.currentTimeMillis()));
+                RecordDB.getInstance().insertRecord(file_name,record_date,file_length,isBookmark,file_name);
             }
         });
-
         return rootView;
     }
-    public static boolean checkWritable() {
-        // Retrieving the external storage state
-        String state = Environment.getExternalStorageState();
-
-        // Check if writable
-        if (Environment.MEDIA_MOUNTED.equals(state)){
-            if(Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
-                return false;
-            }else{
-                return true;
-            }
+    private void setMediaRecorder(){
+        myAudioRecorder = new MediaRecorder();
+        myAudioRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        myAudioRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        myAudioRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+        System.out.println(file_name);
+        myAudioRecorder.setOutputFile(file_name);
+        try {
+            myAudioRecorder.prepare();
         }
-        return false;
+        catch (IllegalStateException ise){
+            System.out.println("에러! : "+ise);
+        }
+        catch (IOException ioe){
+            System.out.println("에러! : "+ioe);
+        }
     }
     private void mediaStart(){
+        stopBtn.setImageResource(R.drawable.stop_enable);
         try{
             myAudioRecorder.start();
         }
         catch (IllegalStateException ise){
             System.out.println(ise);
         }
+        isStart=true;
+        timeThread.start();
     }
-    private String getLength(){
-        int duration=0;
-
-        if(mediaPlayer.isPlaying()) {
-            mediaPlayer.pause();
+    @SuppressLint("HandlerLeak")
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            int sec = (msg.arg1 / 100) % 60;
+            int min = (msg.arg1 / 100) / 60;
+            int hour = (msg.arg1 / 100) / 360;
+            @SuppressLint("DefaultLocale") String result = String.format("%02d:%02d:%02d", hour, min, sec);
+            recordTimeTv.setText(result);
         }
-        mediaPlayer.reset();
-        try {
-            mediaPlayer.setDataSource(file_name);
-
+    };
+    public class timeThread implements Runnable {
+        @Override
+        public void run() {
+            int i = 0;
+            while (true) {
+                while (isRecord) { //일시정지를 누르면 멈춤
+                    Message msg = new Message();
+                    msg.arg1 = i++;
+                    handler.sendMessage(msg);
+                    try {
+                        Thread.sleep(10);
+                    }
+                    catch (InterruptedException e) {
+                        e.printStackTrace();
+                        return; // 인터럽트 받을 경우 return
+                    }
+                }
+            }
         }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        String Hour = String.format("%02d",(duration/1000/3600));
-        String Min = String.format("%02d",((duration/1000/60)%60));
-        String Sec = String.format("%02d", (duration / 1000) % 60);
-        return Hour+":"+Min+":"+Sec;
     }
 }
+
+
